d44e6878a2a90e1765c310ae1d8d7dbd64462140
Anubhav Gupta <anubhav.11803451@lpu.in>
2020-05-07 16:02:10 +0530

    Update splash.dart

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gitub_ui/screens/home.dart';
 import 'package:splashscreen/splashscreen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -7,7 +8,7 @@ class splashscreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SplashScreen(
       seconds: 4,
-      // navigateAfterSeconds: new login(),
+      navigateAfterSeconds: new Homescreen(),
       image: new Image.asset('assets/logo/github1.png'),
       loadingText: Text(
         'Hang In There .. !!',
