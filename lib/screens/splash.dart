06c6ad9267050c58ae8e149fa7f5ec9c7456395e
Anubhav Gupta <anubhav.11803451@lpu.in>
2020-05-07 14:56:54 +0530

    Update splash.dart

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,2 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+class splashscreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 4,
+      navigateAfterSeconds: new Introscreen(),
+      image: new Image.asset('assets/logo/github.png'),
+    );
+  }
+}
