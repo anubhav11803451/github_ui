82c686680b3dc1eca51c7087bf95b948970dc6b8
Anubhav Gupta <anubhav.11803451@lpu.in>
2020-05-08 13:53:21 +0530

    Update splash.dart

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'package:gitub_ui/pages/welcome.dart';
 import 'package:splashscreen/splashscreen.dart';
@@ -11,13 +10,6 @@ class Splashscreen extends StatelessWidget {
       seconds: 4,
       navigateAfterSeconds: new WelcomePage(),
       image: new Image.asset('assets/logo/github4.png'),
-      loadingText: Text(
-        'Hang In There .. !!',
-        style: GoogleFonts.handlee(
-            fontSize: 15,
-            fontWeight: FontWeight.bold,
-            color: Colors.black),
-      ),
       backgroundColor: Colors.white,
       loaderColor: Colors.black,
       photoSize: 55,
