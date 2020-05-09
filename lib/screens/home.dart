a2fa37e82197761f5045f74b140fa010e3a2932c
Anubhav Gupta <anubhav.11803451@lpu.in>
2020-05-09 18:20:39 +0530

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,56 +1,76 @@
 import 'package:flutter/material.dart';
-// import 'package:google_fonts/goo gle_fonts.dart';
 
 import 'package:gitub_ui/screens/homebody.dart';
-
 
 class Homescreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   elevation: 0,
-      //   backgroundColor: Colors.black,
-      //   title: Text('Home', style: GoogleFonts.openSans(color: Colors.white)),
-      //   centerTitle: true,
-      // ),
-      // drawer: Drawer(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        backgroundColor: Colors.black87,
-        elevation: 8,
-        tooltip: 'New Issue',
-        onPressed: () {},
-      ),
+      floatingActionButton: BottomFloatItem(isFactive: true),
       bottomNavigationBar: BottomAppBar(
         elevation: 5.5,
         color: Colors.white,
         shape: CircularNotchedRectangle(),
         notchMargin: 4.0,
-        child: new Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.search,
-                color: Colors.black,
-              ),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: Icon(
-                Icons.account_circle,
-                color: Colors.black,
-              ),
-              onPressed: () {},
-            ),
-          ],
-        ),
+        child: BottomNavItem(),
       ),
       backgroundColor: Colors.grey,
       body: HomeBody(),
+    );
+  }
+}
+
+class BottomFloatItem extends StatelessWidget {
+  final bool isFactive;
+  const BottomFloatItem({
+    Key key,
+    this.isFactive = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      child: Icon(
+        Icons.home,
+        color: isFactive ? Colors.blue : Colors.black,
+      ),
+      backgroundColor: Colors.black87,
+      elevation: 8,
+      tooltip: 'New Issue',
+      onPressed: () {},
+    );
+  }
+}
+
+class BottomNavItem extends StatelessWidget {
+  final bool isActive;
+  const BottomNavItem({
+    Key key,
+    this.isActive = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.search,
+            color: isActive ? Colors.blue : Colors.black,
+          ),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.account_circle,
+            color: Colors.black,
+          ),
+          onPressed: () {},
+        ),
+      ],
     );
   }
 }
