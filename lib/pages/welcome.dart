36698ff516e6918de0f497e839ce25e0ed15fe6f
Anubhav Gupta <anubhav.11803451@lpu.in>
2020-05-11 20:01:25 +0530

    Update welcome.dart

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:gitub_ui/widgets/signupButton.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'package:gitub_ui/widgets/designed_container.dart';
+import 'package:gitub_ui/widgets/signupButton.dart';
 import 'package:gitub_ui/widgets/loginButton.dart';
 
 
