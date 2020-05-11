afed23ec7352a60182472f574295ace9ef20d9d4
Anubhav Gupta <anubhav.11803451@lpu.in>
2020-05-11 12:43:24 +0530

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:gitub_ui/screens/homebody.dart';
+import 'package:gitub_ui/screens/homeScreenBody.dart';
 import 'package:gitub_ui/widgets/bottom_app_bar.dart';
 
 class Homescreen extends StatelessWidget {
@@ -8,8 +8,8 @@ class Homescreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: BottomFloatButton(isFactive: true),
-      bottomNavigationBar: BottomNavBar(),
+      floatingActionButton: BottomFloatButton(isFactive: true), //defined in bottom_app_bar.dart
+      bottomNavigationBar: BottomNavBar(), //defined in bottom_app_bar.dart
       backgroundColor: Colors.grey,
       body: HomeBody(),
     );
